dfd0dfcf6909634dc7ea7c36c6f5844117433912
Bruno Vigidio <bruno.vigidio@farfetch.com>
2024-02-14 17:52:57 +0000

    feat: add CPU complexity to test resource usage

--- src/example-api/Program.cs
+++ src/example-api/Program.cs
@@ -1,7 +1,34 @@
+using System.Collections.Concurrent;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.Urls.Add("http://*:5274");
 
-app.MapGet("/", () => "Hello World4!");
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/prime", () =>
+{
+    var primeNumbers = new ConcurrentBag<int>();
+
+    Parallel.ForEach(Enumerable.Range(1, 1000), num =>
+    {
+        if (IsPrime(num))
+            primeNumbers.Add(num);
+    });
+
+    return $"Found {primeNumbers.Count} prime numbers.";
+});
 
 app.Run();
+return;
+
+bool IsPrime(int number)
+{
+    if (number <= 1) return false;
+    for (var i = 2; i * i <= number; i++)
+    {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
+}
